4cc1bfeb0be1c7388fa095f066fe51e1de7ba8b2
Mohammed Rakib <mohammedrakib944@gmail.com>
2022-12-01 12:33:13 +0600

    title and subtitle added on about

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -3,24 +3,32 @@ import "../../App.css";
 
 const About = () => {
   return (
-    <div className="container pt-10 xl:pt-[100px] pb-10 xl:pb-[100px] grid gap-6 md:grid-cols-2 place-items-center">
-      <div className="treeBG">
-        <img className="w-[300px] md:w-[350px]" src={Tree} alt="" />
-      </div>
-      <div>
-        <h2>
-          Lorem ipsum dolor sit, amet{" "}
-          <span className="p-text">consectetur</span> adipisicing elit.
-        </h2>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet adipisicing elit. Cum laboriosam ab ut
-          deserunt fuga dolorem sequi reprehenderit incidunt non eius, est,
-          recusandae eaque quis nisi amet ex! Architecto, dolore explicabo.
+    <div>
+      <div className="w-full text-center pt-10 px-6">
+        <h3 className="text-2xl">Introducing block2030</h3>
+        <p className="pt-4 text-lg">
+          Decentrally we provide carbon credit for your company
         </p>
-        <br />
-        <br />
-        <button>Read More</button>
+      </div>
+      <div className="container pt-10 xl:pt-[20px] pb-10 xl:pb-[100px] grid gap-6 md:grid-cols-2 place-items-center">
+        <div className="treeBG">
+          <img className="w-[300px] md:w-[350px]" src={Tree} alt="" />
+        </div>
+        <div>
+          <h2>
+            Lorem ipsum dolor sit, amet{" "}
+            <span className="p-text">consectetur</span> adipisicing elit.
+          </h2>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet adipisicing elit. Cum laboriosam ab ut
+            deserunt fuga dolorem sequi reprehenderit incidunt non eius, est,
+            recusandae eaque quis nisi amet ex! Architecto, dolore explicabo.
+          </p>
+          <br />
+          <br />
+          <button>Read More</button>
+        </div>
       </div>
     </div>
   );
