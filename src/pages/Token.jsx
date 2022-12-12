884c199e6bc8b0329e516a1c56511a63f674b214
Rashid Shahriar <rashidslink@gmail.com>
2022-12-12 11:53:37 +0600

    token and partnership page

--- src/pages/Token.jsx
+++ src/pages/Token.jsx
@@ -1,7 +1,7 @@
 import COIN from "../img/others/coin-g.png";
 import roundimg1 from "../img/others/r1.png";
-import roundimg2 from "../img/others/r2.png";
 import roundimg3 from "../img/others/r3.png";
+import midimg from "../img/others/image12.png";
 import BG from "../img/others/tree-bg.png";
 import "./token.css";
 
@@ -27,212 +27,168 @@ const Token = () => {
               alt="background"
             />{" "}
           </div>
-          {/* <img
-            src={BG}
-            className="w-full h-[460px] object-cover absolute mix-blend-overlay"
-            alt="background"
-          /> */}
 
           <div className="flex flex-col min-h-[460px] justify-center items-center gap-y-6 md:gap-y-10">
-            <h1 className="text-[40px] sm:text-[50px] mb-7 text-white font-poppins">
-              Carbon X Crypto
-            </h1>
+            <h1 className="text-[50px] mb-7 text-white font-poppins">Token</h1>
           </div>
         </div>{" "}
       </section>
       <section id="about" className="mx-3 md:mx-0">
-        <div className="flex flex-col md:flex-row md:justify-around gap-4 pt-20">
-          <div className="mx-4 sm:mt-0 flex flex-col md:justify-start md:items-start justify-center items-center">
-            <div className="flex flex-col md:justify-start md:items-start justify-center items-center text-4xl md:text-6xl gap-y-4 mt-0 md:mt-10">
+        <div className="flex flex-col md:flex-row md:justify-around gap-4 pt-10">
+          <div className="mx-4 sm:mt-0 flex flex-col md:items-start justify-center items-center">
+            {/* <div className="flex flex-col md:justify-start md:items-start justify-center items-center text-4xl md:text-6xl gap-y-4 mt-0 md:mt-10">
               <span className="font-medium">Lorem Ipsum</span>
               <span className="font-medium text-[#0DCB6B]">Dolor Sit</span>
-            </div>
-            <div className="max-w-[300px] pt-8">
+            </div> */}
+            <div className="max-w-[300px] pt-8 text-xl font-normal">
               <p>
-                Lorem ipsum dolor sit amet consectetur. Nisi odio placerat
-                pellentesque dictum viverra. Ac quam curabitur aliquet cursus in
-                justo nunc.
+                Our innovative two-token structure makes offsetting and
+                investing in the voluntary carbon market simple and accessible
+                for all.
               </p>
             </div>
-            <div
-              className="flex flex-col gap-y-6 
-          pt-8"
-            >
+            <div className="flex flex-col gap-y-6 pt-8">
               <button className="flex justify-center  text-white text-base md:text-xl font-normal rounded-3xl py-2 w-[180px] bg-gradient-to-r from-[#20972A] to-[#0FC766]">
-                Lite Paper
+                Whitepaper
               </button>
               <button className="flex justify-center text-white text-base md:text-xl font-normal rounded-3xl py-2 w-[180px] bg-gradient-to-r from-[#40559E] to-[#0FC7BC]">
-                FAQs
+                FAQ
               </button>
             </div>
           </div>
-          <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 md:gap-2 pt-10 md:pt-0">
-              <div className="grid  w-full place-content-center ">
-                <div
-                  className="flex h-[300px] md:h-[400px] min-w-[300px]  md:min-w-[150px] md:max-w-[180px] 
-                flex-col justify-around rounded-xl md:rounded-full  p-4 shadow-lg 
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5 md:gap-2 pt-10 md:pt-0">
+            <div className="grid  w-full place-content-center">
+              <div
+                className="flex h-[300px] md:h-[400px] min-w-[300px] md:min-w-[150px] md:max-w-[200px] 
+                flex-col justify-evenly  rounded-xl md:rounded-full m-2 p-4 shadow-lg 
                 items-center border-[1px] "
-                >
-                  <p className="mt-0 md:mt-6 text-center text-base pb-8 md:pb-0">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Repudiandae quasi quod beatae quia corrupti deleniti
-                    voluptatibus incidunt aspernatur.
-                  </p>
-                  <div class=" w-[150px]  md:w-[150px] p-4">
-                    <img
-                      src={roundimg1}
-                      alt="..."
-                      class="max-w-full h-auto align-middle border-none"
-                    />
-                  </div>
-                </div>
+              >
+                <img
+                  src={roundimg1}
+                  alt="..."
+                  className="w-[200px] align-middle border-none"
+                />
+
+                <p className="text-center text-base mb-6">
+                  The Block token allows investors to gain exposure to the
+                  voluntary carbon credit market. This is the first time
+                  investors can easily access this market, making the Block
+                  token valuable and innovative.
+                </p>
               </div>
-              <div className="grid  w-full place-content-center">
-                <div
-                  className="flex-col-reverse flex h-[300px] md:h-[500px] min-w-[300px] md:min-w-[150px] md:max-w-[180px] 
-                md:flex-col rounded-xl md:rounded-full  p-4 shadow-lg items-center border-[1px]
-                justify-around "
-                >
-                  <div class=" w-[150px]  md:w-[150px] p-4">
-                    <img
-                      src={roundimg2}
-                      alt="..."
-                      class="max-w-full align-middle border-none h-auto"
-                    />
-                  </div>
-                  <p className=" text-center text-base">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Repudiandae quasi quod beatae quia corrupti deleniti
-                    voluptatibus incidunt aspernatur. andae quasi quod beatae
-                    quia corrupti deleniti
-                  </p>
-                </div>
-              </div>
-              <div className="grid  w-full place-content-center">
-                <div
-                  className="flex h-[300px] md:h-[400px] min-w-[300px] md:min-w-[150px] md:max-w-[180px] 
-                flex-col justify-around rounded-xl md:rounded-full  p-4 shadow-lg 
+            </div>
+
+            <div className="grid  w-full place-content-center">
+              <div
+                className="flex h-[300px] md:h-[450px] min-w-[300px] md:min-w-[150px] md:max-w-[200px] 
+                flex-col justify-evenly  rounded-xl md:rounded-full  m-2 p-4 shadow-lg 
                 items-center border-[1px] "
-                >
-                  <p className="mt-0 md:mt-6 text-center text-base pb-8 md:pb-0">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Repudiandae quasi quod beatae quia corrupti deleniti
-                    voluptatibus incidunt aspernatur.
-                  </p>
-                  <div class=" w-[150px]  md:w-[150px] p-4">
-                    <img
-                      src={roundimg3}
-                      alt="..."
-                      class=" max-w-full h-auto align-middle border-none"
-                    />
-                  </div>
-                </div>
+              >
+                <img
+                  src={roundimg3}
+                  alt="..."
+                  className=" w-[200px] align-middle border-none"
+                />
+
+                <p className="text-center text-base mb-6">
+                  The CO2 token is a digital asset backed by carbon credits. It
+                  allows businesses to of offset their emissions in a
+                  transparent and accessible way. When the token is bought, it
+                  is burned and the underlying carbon credits are transferred to
+                  the purchaser.
+                </p>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section id="last" className="mx-3 md:mx-0">
-        <div className="flex justify-center font-medium text-3xl sm:text-5xl pt-36">
-          <h1>
-            Lorem Ipsum
-            <span className="text-[#0DCB6B] pl-2">Dolor sit</span>
+
+      <section id="middle" className="my-20 h-full max-w-[1200px] mx-auto">
+        <hr className="max-w-[1200px] mx-auto" />
+        <div className="flex flex-col md:flex-row justify-evenly items-center gap-y-5 md:gap-y-0 py-16 md:py-24">
+          <div className="max-w-[300px] md:max-w-[400px] text-xl font-normal flex border border-gray-200 shadow-md rounded-2xl">
+            <p className="m-6">
+              With every offset, a fee will go into the block token giving
+              individuals exposure to the growing market through the block token
+            </p>
+          </div>
+          <img src={midimg} alt="" className="max-w-[300px] md:max-w-[500px]" />
+        </div>
+        <hr className="max-w-[1200px] mx-auto" />
+      </section>
+
+      <section id="last" className="mx-3 md:mx-0 my-10">
+        <div className="flex justify-center items-center font-medium ">
+          <h1 className=" text-4xl md:text-5xl text-center">
+            The Market Is
+            <span className="text-[#0DCB6B] pl-2">Growing Quickly</span>
           </h1>
         </div>
-        <div className="flex justify-center text-xl font-normal pt-8 ">
+        <div className="flex justify-center text-xl font-normal pt-8">
           <p className="max-w-[1000px] text-center">
-            Lorem ipsum dolor sit amet consectetur. Nisi odio placerat
-            pellentesque dictum viverra. Ac quam curabitur aliquet cursus in
-            justo nunc.
+            More and more companies offset their emissions through voluntary
+            varbon credits
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-5 md:gap-4 pt-24 place-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-5 md:gap-2 pt-16 place-items-center">
           <div>
-            <div>
+            <div className="">
               <img src={COIN} alt="" srcSet="" />
-            </div>
-            <div className="bg-gradient-to-r from-[#419E40] to-[#12C2BB]">
-              <h3 className="text-center py-3 text-white">
-                1 token = Tonnes of Carbon
-              </h3>
             </div>
           </div>
           <div className="flex flex-col gap-y-4 mt-14 md:mt-0">
-            <div>
-              <h1
-                href="#"
-                className="block p-6  max-w-[24rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200"
-              >
-                <p className="mb-2 text-4xl font-medium text-[#219325] tracking-tigh text-center">
-                  Lorem ipsum
-                </p>
-                <p className="font-normal pt-6 text-base text-justify">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel
-                  quam congue nuncLorem ipsum dolor sit amet, consectetur
-                  adipiscing elit. Vel quam congue nuncLorem ipsum dolor
-                </p>
-              </h1>
-            </div>
+            <h1
+              href="#"
+              className=" p-6  max-w-[24rem] h-[10rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200 hover:shadow-lg"
+            >
+              <p className="mb-2 text-3xl font-medium text-[#219325] tracking-tigh text-center">
+                High Demand
+              </p>
+              <p className="font-normal pt-6 text-base ">
+                Demand is set to grow 15x by 2030 (McKinsey)
+              </p>
+            </h1>
 
-            <div>
-              <h1
-                href="#"
-                className="block p-6  max-w-[24rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200"
-              >
-                <p className="mb-2 text-4xl font-medium text-[#219325] tracking-tigh text-center">
-                  Lorem ipsum
-                </p>
-                <p className="font-normal text-justify pt-6 text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel
-                  quam congue nuncLorem ipsum dolor sit amet, consectetur
-                  adipiscing elit. Vel quam congue nuncLorem ipsum dolor
-                </p>
-              </h1>
-            </div>
+            <h1
+              href="#"
+              className=" p-6  max-w-[24rem] h-[10rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200  hover:shadow-lg"
+            >
+              <p className="mb-2 text-3xl font-medium text-[#219325] tracking-tigh text-center">
+                Net zero commitments
+              </p>
+              <p className="font-normal  pt-6 text-base">
+                Over 2,000 companies announce net-zero ambitions
+              </p>
+            </h1>
           </div>
 
           <div className="flex flex-col gap-y-4">
-            <div>
-              <h1
-                href="#"
-                className="block p-6  max-w-[24rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200"
-              >
-                <p className="mb-2 text-4xl font-medium text-[#219325] tracking-tigh text-center">
-                  Lorem ipsum
-                </p>
-                <p className="font-normal text-justify pt-6 text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel
-                  quam congue nuncLorem ipsum dolor sit amet, consectetur
-                  adipiscing elit. Vel quam congue nuncLorem ipsum dolor
-                </p>
-              </h1>
-            </div>
+            <h1
+              href="#"
+              className=" p-6  max-w-[24rem] h-[10rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200  hover:shadow-lg"
+            >
+              <p className="mb-2 text-3xl font-medium text-[#219325] tracking-tigh text-center">
+                Huge market
+              </p>
+              <p className="font-normal  pt-6 text-base">
+                Corporate market is expected to grow to $30B in 2030
+              </p>
+            </h1>
 
-            <div>
-              <h1
-                href="#"
-                className="block p-6  max-w-[24rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200"
-              >
-                <p className="mb-2 text-4xl font-medium text-[#219325] tracking-tigh text-center">
-                  Lorem ipsum
-                </p>
-                <p className="font-normal text-justify pt-6 text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel
-                  quam congue nuncLorem ipsum dolor sit amet, consectetur
-                  adipiscing elit. Vel quam congue nuncLorem ipsum dolor
-                </p>
-              </h1>
-            </div>
+            <h1
+              href="#"
+              className="p-6  max-w-[24rem] h-[10rem] bg-white rounded-2xl border border-gray-200 shadow-md  dark:shadow-gray-200  hover:shadow-lg"
+            >
+              <p className="mb-2 text-3xl font-medium text-[#219325] tracking-tigh text-center">
+                Low Supply
+              </p>
+              <p className="font-normal pt-6 text-base">
+                Credits take a long time to be issued
+              </p>
+            </h1>
           </div>
-        </div>
-        <div className="flex justify-center text-xl font-normal pt-24 ">
-          <p className="text-center max-w-[860px]">
-            Lorem ipsum dolor sit amet consectetur. Nisi odio placerat
-            pellentesque dictum viverra. Ac quam curabitur aliquet cursus in
-            justo nunc.
-          </p>
         </div>
       </section>
     </div>
